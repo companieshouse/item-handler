2c47486c4044e408cdbb8e902a88ba1abefe8fdb
rjadhav <rjadhav@companieshouse.gov.uk>
2020-04-14 06:18:05 +0100

    Revert "Add AOP based test for Kafka consumer"
    This reverts commit 1f9b2209e81ada029d512144efb2fd42368a3326.

--- src/test/java/uk/gov/companieshouse/itemhandler/ItemHandlerApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/itemhandler/ItemHandlerApplicationTests.java
@@ -2,10 +2,8 @@ package uk.gov.companieshouse.itemhandler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka
 class ItemHandlerApplicationTests {
 
 	@Test
--- src/test/java/uk/gov/companieshouse/itemhandler/controller/HealthcheckControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/itemhandler/controller/HealthcheckControllerIntegrationTest.java
@@ -5,15 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@EmbeddedKafka
 class HealthcheckControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
