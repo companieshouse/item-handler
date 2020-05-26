c1a3014fe21f3997f74d55accfbb11cfb3e9e006
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-05-26 10:28:25 +0100

    GCI-931: Initial delivery details mapping.

--- src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrderDataToCertificateOrderConfirmationMapper.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrderDataToCertificateOrderConfirmationMapper.java
@@ -1,10 +1,20 @@
 package uk.gov.companieshouse.itemhandler.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import uk.gov.companieshouse.itemhandler.email.CertificateOrderConfirmation;
 import uk.gov.companieshouse.itemhandler.model.OrderData;
 
 @Mapper(componentModel = "spring")
 public interface OrderDataToCertificateOrderConfirmationMapper {
+
+    @Mapping(source = "deliveryDetails.forename", target="forename")
+    @Mapping(source = "deliveryDetails.surname", target="surname")
+    @Mapping(source = "deliveryDetails.addressLine1", target="addressLine1")
+    @Mapping(source = "deliveryDetails.addressLine2", target="addressLine2")
+    @Mapping(source = "deliveryDetails.locality", target="houseName")
+    @Mapping(source = "deliveryDetails.premises", target="houseNumberStreetName")
+    @Mapping(source = "deliveryDetails.region", target="city")
+    @Mapping(source = "deliveryDetails.postalCode", target="postCode")
     CertificateOrderConfirmation orderToConfirmation(OrderData order);
 }
