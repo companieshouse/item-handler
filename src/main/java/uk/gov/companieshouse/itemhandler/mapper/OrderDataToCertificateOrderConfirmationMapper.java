ced766cf4c8f71b641fae940b6cc488abfdfc616
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-05-26 15:26:28 +0100

    GCI-931: Map order details fields.

--- src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrderDataToCertificateOrderConfirmationMapper.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrderDataToCertificateOrderConfirmationMapper.java
@@ -1,12 +1,19 @@
 package uk.gov.companieshouse.itemhandler.mapper;
 
+import org.apache.commons.lang.WordUtils;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import uk.gov.companieshouse.itemhandler.email.CertificateOrderConfirmation;
 import uk.gov.companieshouse.itemhandler.model.OrderData;
 
+import java.time.format.DateTimeFormatter;
+
 @Mapper(componentModel = "spring")
 public interface OrderDataToCertificateOrderConfirmationMapper {
+
+    DateTimeFormatter TIME_OF_PAYMENT_FORMATTER = DateTimeFormatter.ofPattern("dd MMMM yyyy 'at' hh:mm");
 
     @Mapping(source = "deliveryDetails.forename", target="forename")
     @Mapping(source = "deliveryDetails.surname", target="surname")
@@ -16,5 +23,17 @@ public interface OrderDataToCertificateOrderConfirmationMapper {
     @Mapping(source = "deliveryDetails.premises", target="houseNumberStreetName")
     @Mapping(source = "deliveryDetails.region", target="city")
     @Mapping(source = "deliveryDetails.postalCode", target="postCode")
+
+    @Mapping(source = "reference", target="orderReferenceNumber")
+    @Mapping(source = "orderedBy.email", target="emailAddress")
+    @Mapping(source = "totalOrderCost", target="feeAmount")
     CertificateOrderConfirmation orderToConfirmation(OrderData order);
+
+    @AfterMapping
+    default void specialMappings(final OrderData order, final @MappingTarget CertificateOrderConfirmation confirmation)
+    {
+        final String timescale = order.getItems().get(0).getItemOptions().getDeliveryTimescale().toString();
+        confirmation.setDeliveryMethod(WordUtils.capitalizeFully(timescale));
+        confirmation.setTimeOfPayment(TIME_OF_PAYMENT_FORMATTER.format(order.getOrderedAt()));
+    }
 }
