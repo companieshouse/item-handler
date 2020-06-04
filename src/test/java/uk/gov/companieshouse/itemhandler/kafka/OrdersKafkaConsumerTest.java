759a0465351add4e211b6c29b0dc9ed1200caeeb
caismith-ch <csmith2@companieshouse.gov.uk>
2020-06-04 15:35:17 +0100

    correct test method name

--- src/test/java/uk/gov/companieshouse/itemhandler/kafka/OrdersKafkaConsumerTest.java
+++ src/test/java/uk/gov/companieshouse/itemhandler/kafka/OrdersKafkaConsumerTest.java
@@ -81,7 +81,7 @@ public class OrdersKafkaConsumerTest {
     }
 
     @Test
-    public void republishMessageToRetryTopicThrowsInterruptedException()
+    public void republishMessageToRetryTopicThrowsSerializationException()
         throws ExecutionException, InterruptedException, SerializationException {
         // Given & When
         when(serializerFactory.getGenericRecordSerializer(OrderReceived.class)).thenReturn(serializer);
