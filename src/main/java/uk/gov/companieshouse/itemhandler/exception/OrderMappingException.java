7ebd5b1cd6c0ce3bdea03c330a2bf2fc1efef48d
rjadhav <rjadhav@companieshouse.gov.uk>
2020-08-25 14:53:51 +0100

    Add OrderMappingException to handle order to order confirmation mapping failure

---
+++ src/main/java/uk/gov/companieshouse/itemhandler/exception/OrderMappingException.java
@@ -0,0 +1,5 @@
+package uk.gov.companieshouse.itemhandler.exception;
+
+public class OrderMappingException extends RuntimeException{
+    public OrderMappingException(String message) { super(message); }
+}
