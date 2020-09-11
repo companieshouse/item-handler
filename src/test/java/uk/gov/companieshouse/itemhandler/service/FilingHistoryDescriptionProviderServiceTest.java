f741403a360a0207980ebcb198ac37ee86ae7a09
nabeelamirch <namir@companieshouse.gov.uk>
2020-09-11 11:26:51 +0100

    ignore objects in filing history description values

--- src/test/java/uk/gov/companieshouse/itemhandler/service/FilingHistoryDescriptionProviderServiceTest.java
+++ src/test/java/uk/gov/companieshouse/itemhandler/service/FilingHistoryDescriptionProviderServiceTest.java
@@ -20,6 +20,10 @@ public class FilingHistoryDescriptionProviderServiceTest {
     private static final Map<String, Object> DESCRIPTION_VALUES_LEGACY;
     private static final String EXPECTED_DESCRIPTION_LEGACY = "This is the description";
 
+    private static final String DESCRIPTION_KEY_ARRAY = "capital-allotment-shares";
+    private static final Map<String, Object> DESCRIPTION_VALUES_ARRAY;
+    private static final String EXPECTED_DESCRIPTION_ARRAY = "Statement of capital following an allotment of shares on 10 November 2019";
+
 
     static {
         DESCRIPTION_VALUES = new HashMap<>();
@@ -28,6 +32,13 @@ public class FilingHistoryDescriptionProviderServiceTest {
 
         DESCRIPTION_VALUES_LEGACY = new HashMap<>();
         DESCRIPTION_VALUES_LEGACY.put("description", "This is the description");
+
+        DESCRIPTION_VALUES_ARRAY = new HashMap<>();
+        DESCRIPTION_VALUES_ARRAY.put("date", "2019-11-10");
+        Map<String, String> capital = new HashMap<>();
+        capital.put("figure", "34,253,377");
+        capital.put("currency", "GBP");
+        DESCRIPTION_VALUES_ARRAY.put("capital", capital);
     }
 
     @Test
@@ -42,6 +53,13 @@ public class FilingHistoryDescriptionProviderServiceTest {
     void mapFilingHistoryDescriptionGetsDescriptionAndReturnsDescriptionInDescriptionValue() {
         final FilingHistoryDescriptionProviderService provider = new FilingHistoryDescriptionProviderService();
         assertThat(provider.mapFilingHistoryDescription(DESCRIPTION_KEY_LEGACY, DESCRIPTION_VALUES_LEGACY), is(EXPECTED_DESCRIPTION_LEGACY));
+    }
+
+    @Test
+    @DisplayName("Ignore description value if it is not a String")
+    void mapFilingHistoryDescriptionIgnoresDescriptionValuesThatAreNotStrings() {
+        final FilingHistoryDescriptionProviderService provider = new FilingHistoryDescriptionProviderService();
+        assertThat(provider.mapFilingHistoryDescription(DESCRIPTION_KEY_ARRAY, DESCRIPTION_VALUES_ARRAY), is(EXPECTED_DESCRIPTION_ARRAY));
     }
 
     @Test
