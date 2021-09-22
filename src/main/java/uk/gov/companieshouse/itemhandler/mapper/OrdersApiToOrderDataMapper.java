d88f1967da6d16666e7ba8d3f15f966750447eea
phudson-ch <phudson@companieshouse.gov.uk>
2021-09-22 18:14:46 +0100

    Fix CompanyType enum mapping problem
    * Upgraded to MapStruct 1.4.1
    * Implemented MapStruct mapping solution for CompanyType enum

--- src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrdersApiToOrderDataMapper.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/mapper/OrdersApiToOrderDataMapper.java
@@ -3,7 +3,11 @@ package uk.gov.companieshouse.itemhandler.mapper;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.mapstruct.ValueMapping;
+import org.mapstruct.ValueMappings;
 import uk.gov.companieshouse.api.model.order.OrdersApi;
 import uk.gov.companieshouse.api.model.order.item.BaseItemApi;
 import uk.gov.companieshouse.api.model.order.item.BaseItemOptionsApi;
@@ -12,11 +16,15 @@ import uk.gov.companieshouse.api.model.order.item.CertifiedCopyItemOptionsApi;
 import uk.gov.companieshouse.api.model.order.item.MissingImageDeliveryItemOptionsApi;
 import uk.gov.companieshouse.itemhandler.model.CertificateItemOptions;
 import uk.gov.companieshouse.itemhandler.model.CertifiedCopyItemOptions;
+import uk.gov.companieshouse.itemhandler.model.CompanyType;
 import uk.gov.companieshouse.itemhandler.model.Item;
 import uk.gov.companieshouse.itemhandler.model.ItemOptions;
 import uk.gov.companieshouse.itemhandler.model.ItemType;
 import uk.gov.companieshouse.itemhandler.model.MissingImageDeliveryItemOptions;
 import uk.gov.companieshouse.itemhandler.model.OrderData;
+
+import static uk.gov.companieshouse.itemhandler.model.CompanyType.LIMITED_LIABILITY_PARTNERSHIP;
+import static uk.gov.companieshouse.itemhandler.model.CompanyType.LIMITED_PARTNERSHIP;
 
 @Mapper(componentModel = "spring")
 public interface OrdersApiToOrderDataMapper {
@@ -53,4 +61,11 @@ public interface OrdersApiToOrderDataMapper {
     CertificateItemOptions apiToCertificateItemOptions(CertificateItemOptionsApi certificateItemOptionsApi);
     CertifiedCopyItemOptions apiToCertifiedCopyItemOptions(CertifiedCopyItemOptionsApi certifiedCopyItemOptionsApi);
     MissingImageDeliveryItemOptions apiToMissingImageDeliveryOptions(MissingImageDeliveryItemOptionsApi missingImageDeliveryItemOptionsApi);
+    @ValueMappings({
+        @ValueMapping(source = "llp", target = "LIMITED_LIABILITY_PARTNERSHIP"),
+        @ValueMapping(source = "limited-partnership", target = "LIMITED_PARTNERSHIP"),
+        @ValueMapping(source = MappingConstants.ANY_UNMAPPED, target = "OTHER")
+    })
+    CompanyType mapCompanyType(String companyType);
+
 }
