294f15793d5ca989a6611f9bc2ed065adbcf801d
phudson-ch <phudson@companieshouse.gov.uk>
2022-03-08 18:51:38 +0000

    Update class diagrams
    * Also deleted unused exception class

--- src/main/java/uk/gov/companieshouse/itemhandler/exception/SendKafkaMessageException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.companieshouse.itemhandler.exception;
-
-public class SendKafkaMessageException extends RetryableException {
-
-    public SendKafkaMessageException(String message) {
-        super(message);
-    }
-}
