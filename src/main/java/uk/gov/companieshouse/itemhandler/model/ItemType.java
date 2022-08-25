11c88dbc3708db14db9c5eeb6858c316928c80e5
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-08-25 09:55:45 +0100

    Align api-enums with integration branch
    * Bring api-enumerations submodule up-to-date with master.
    * Fix failing tests.

--- src/main/java/uk/gov/companieshouse/itemhandler/model/ItemType.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/model/ItemType.java
@@ -16,7 +16,7 @@ public enum ItemType {
     MISSING_IMAGE_DELIVERY("item#missing-image-delivery") {
         @Override
         public void sendMessages(OrderData order) {
-            getItemSender().sendItemsToChd(null);
+            getItemSender().sendItemsToChd(new ItemGroup(order, "item#missing-image-delivery"));
         }
     };
 
