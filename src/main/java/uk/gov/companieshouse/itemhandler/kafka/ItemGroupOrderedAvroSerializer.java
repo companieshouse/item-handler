d1505e4719d4bed9c992eb54243cde3af6c55df0
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-10-17 17:03:37 +0100

    DCAC-254: Test and improve serializer error logging and propagation.

--- src/main/java/uk/gov/companieshouse/itemhandler/kafka/ItemGroupOrderedAvroSerializer.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/kafka/ItemGroupOrderedAvroSerializer.java
@@ -7,11 +7,16 @@ import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 import uk.gov.companieshouse.itemgroupordered.ItemGroupOrdered;
+import uk.gov.companieshouse.itemhandler.logging.LoggingUtils;
+import uk.gov.companieshouse.logging.Logger;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public class ItemGroupOrderedAvroSerializer implements Serializer<ItemGroupOrdered> {
+
+    // Logger cannot be injected because Kafka code uses a public 0-args
+    // constructor to instantiate this serializer.
+    private static final Logger LOGGER = LoggingUtils.getLogger();
 
     @Override
     public byte[] serialize(String topic, ItemGroupOrdered data) {
@@ -27,8 +32,10 @@ public class ItemGroupOrderedAvroSerializer implements Serializer<ItemGroupOrder
             encoder.flush();
 
             return serializedData;
-        } catch (IOException e) {
-            throw new SerializationException("Error when serializing ItemGroupOrdered to byte[]");
+        } catch (Exception e) {
+            final String error = "Error when serializing ItemGroupOrdered to byte[], error: " + e.getMessage();
+            LOGGER.error(error);
+            throw new SerializationException(error);
         }
     }
 }
