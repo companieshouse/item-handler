213d6649f07f884d4e56ab5a841bd17c2221b557
Cai Smith <csmith2@companieshouse.gov.uk>
2024-04-08 17:20:15 +0100

    Revert "Remove flakey integration tests"

---
+++ src/test/java/uk/gov/companieshouse/itemhandler/kafka/OrderMessageErrorConsumerIntegrationTest.java
@@ -0,0 +1,248 @@
+package uk.gov.companieshouse.itemhandler.kafka;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import email.email_send;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.IOUtils;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.model.JsonBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.utility.DockerImageName;
+import uk.gov.companieshouse.itemhandler.config.EmbeddedKafkaBrokerConfiguration;
+import uk.gov.companieshouse.itemhandler.config.TestEnvironmentSetupHelper;
+import uk.gov.companieshouse.itemhandler.service.EmailService;
+import uk.gov.companieshouse.itemhandler.util.TestConstants;
+import uk.gov.companieshouse.orders.OrderReceived;
+import uk.gov.companieshouse.orders.items.ChdItemOrdered;
+
+@SpringBootTest
+@Import(EmbeddedKafkaBrokerConfiguration.class)
+@TestPropertySource(locations = "classpath:application.properties",
+        properties = {"uk.gov.companieshouse.item-handler.error-consumer=true"})
+class OrderMessageErrorConsumerIntegrationTest {
+
+    private static int orderId = 123456;
+    private static MockServerContainer container;
+    private MockServerClient client;
+
+    @Autowired
+    private KafkaConsumer<String, email_send> emailSendConsumer;
+
+    @Autowired
+    private KafkaConsumer<String, ChdItemOrdered> chsItemOrderedConsumer;
+
+    @Autowired
+    private KafkaProducer<String, OrderReceived> orderReceivedProducer;
+
+    @Autowired
+    private KafkaConsumer<String, OrderReceived> orderReceivedRetryConsumer;
+
+    @Autowired
+    private OrderMessageErrorConsumerAspect orderMessageErrorConsumerAspect;
+
+    @Autowired
+    private KafkaTopics kafkaTopics;
+
+    @Autowired
+    private PartitionOffset errorRecoveryOffset;
+
+    @Autowired
+    private ErrorConsumerController errorConsumerController;
+
+    @BeforeAll
+    static void before() {
+        container = new MockServerContainer(DockerImageName.parse(
+                "jamesdbloom/mockserver:mockserver-5.5.4"));
+        container.start();
+        TestEnvironmentSetupHelper.setEnvironmentVariable("API_URL",
+                "http://" + container.getHost() + ":" + container.getServerPort());
+        TestEnvironmentSetupHelper.setEnvironmentVariable("CHS_API_KEY", "123");
+        TestEnvironmentSetupHelper.setEnvironmentVariable("PAYMENTS_API_URL",
+                "http://" + container.getHost() + ":" + container.getServerPort());
+    }
+
+    @AfterAll
+    static void after() {
+        container.stop();
+    }
+    
+    @BeforeEach
+    void setup() {
+        client = new MockServerClient(container.getHost(), container.getServerPort());
+        errorRecoveryOffset.reset();
+        errorConsumerController.resumeConsumerThread();
+    }
+
+    @AfterEach
+    void teardown() {
+        client.reset();
+        ++orderId;
+    }
+
+    @Test
+    void testConsumesCertificateOrderReceivedFromErrorTopic() throws
+            ExecutionException, InterruptedException,
+            IOException {
+        //given
+        client.when(request()
+                        .withPath(getOrderReference())
+                        .withMethod(HttpMethod.GET.toString()))
+                .respond(response()
+                        .withStatusCode(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .withBody(JsonBody.json(IOUtils.resourceToString(
+                                "/fixtures/certified-certificate.json",
+                                StandardCharsets.UTF_8))));
+        orderMessageErrorConsumerAspect.setBeforeProcessOrderReceivedEventLatch(new CountDownLatch(1));
+        orderMessageErrorConsumerAspect.setAfterOrderConsumedEventLatch(new CountDownLatch(1));
+
+        //when
+        ProducerRecord<String, OrderReceived> producerRecord = new ProducerRecord<>(
+                kafkaTopics.getOrderReceivedError(),
+                kafkaTopics.getOrderReceivedError(),
+                getOrderReceived());
+        orderReceivedProducer.send(producerRecord).get();
+        orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().countDown();
+        orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().await(30, TimeUnit.SECONDS);
+        email_send actual = KafkaTestUtils.getSingleRecord(emailSendConsumer, kafkaTopics.getEmailSend()).value();
+
+        //then
+        assertEquals(0, orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().getCount());
+        assertEquals(0, orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().getCount());
+        assertEquals(TestConstants.CERTIFICATE_ORDER_NOTIFICATION_API_APP_ID, actual.getAppId());
+        assertNotNull(actual.getMessageId());
+        assertEquals(TestConstants.CERTIFICATE_ORDER_NOTIFICATION_API_MESSAGE_TYPE,
+                actual.getMessageType());
+        assertEquals(EmailService.TOKEN_EMAIL_ADDRESS, actual.getEmailAddress());
+        assertNotNull(actual.getData());
+    }
+
+    @Test
+    void testConsumesCertifiedDocumentOrderReceivedFromErrorTopic() throws ExecutionException, InterruptedException, IOException {
+        //given
+        client.when(request()
+                        .withPath(getOrderReference())
+                        .withMethod(HttpMethod.GET.toString()))
+                .respond(response()
+                        .withStatusCode(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .withBody(JsonBody.json(IOUtils.resourceToString(
+                                "/fixtures/certified-copy.json",
+                                StandardCharsets.UTF_8))));
+        orderMessageErrorConsumerAspect.setBeforeProcessOrderReceivedEventLatch(new CountDownLatch(1));
+        orderMessageErrorConsumerAspect.setAfterOrderConsumedEventLatch(new CountDownLatch(1));
+
+        //when
+        ProducerRecord<String, OrderReceived> producerRecord = new ProducerRecord<>(
+                kafkaTopics.getOrderReceivedError(),
+                kafkaTopics.getOrderReceivedError(),
+                getOrderReceived());
+        orderReceivedProducer.send(producerRecord).get();
+        orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().countDown();
+        orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().await(30, TimeUnit.SECONDS);
+        email_send actual = KafkaTestUtils.getSingleRecord(emailSendConsumer, kafkaTopics.getEmailSend()).value();
+
+        //then
+        assertEquals(0, orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().getCount());
+        assertEquals(0, orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().getCount());
+        assertEquals(TestConstants.CERTIFIED_COPY_ORDER_NOTIFICATION_API_APP_ID, actual.getAppId());
+        assertNotNull(actual.getMessageId());
+        assertEquals(TestConstants.CERTIFIED_COPY_ORDER_NOTIFICATION_API_MESSAGE_TYPE,
+                actual.getMessageType());
+        assertEquals(EmailService.TOKEN_EMAIL_ADDRESS, actual.getEmailAddress());
+        assertNotNull(actual.getData());
+    }
+
+    @Test
+    void testConsumesMissingImageDeliveryFromNotificationErrorAndPublishesChsItemOrdered() throws ExecutionException, InterruptedException, IOException {
+        //given
+        client.when(request()
+                        .withPath(getOrderReference())
+                        .withMethod(HttpMethod.GET.toString()))
+                .respond(response()
+                        .withStatusCode(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .withBody(JsonBody.json(IOUtils.resourceToString(
+                                "/fixtures/missing-image-delivery.json",
+                                StandardCharsets.UTF_8))));
+        orderMessageErrorConsumerAspect.setBeforeProcessOrderReceivedEventLatch(new CountDownLatch(1));
+        orderMessageErrorConsumerAspect.setAfterOrderConsumedEventLatch(new CountDownLatch(1));
+
+        //when
+        ProducerRecord<String, OrderReceived> producerRecord = new ProducerRecord<>(
+                kafkaTopics.getOrderReceivedError(),
+                kafkaTopics.getOrderReceivedError(),
+                getOrderReceived());
+        orderReceivedProducer.send(producerRecord).get();
+        orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().countDown();
+        orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().await(30, TimeUnit.SECONDS);
+        ChdItemOrdered actual = KafkaTestUtils.getSingleRecord(chsItemOrderedConsumer, kafkaTopics.getChdItemOrdered()).value();
+
+        //then
+        assertEquals(0, orderMessageErrorConsumerAspect.getBeforeProcessOrderReceivedEventLatch().getCount());
+        assertEquals(0, orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().getCount());
+        assertEquals("ORD-123123-123123", actual.getReference());
+        assertNotNull(actual.getItem());
+    }
+
+    @Test
+    void testPublishesOrderReceivedToRetryTopicWhenOrdersApiIsUnavailable() throws ExecutionException, InterruptedException, IOException {
+        //given
+        client.when(request()
+                        .withPath(getOrderReference())
+                        .withMethod(HttpMethod.GET.toString()))
+                .respond(response()
+                        .withStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        orderMessageErrorConsumerAspect.setAfterOrderConsumedEventLatch(new CountDownLatch(1));
+
+        // when
+        ProducerRecord<String, OrderReceived> producerRecord = new ProducerRecord<>(
+                kafkaTopics.getOrderReceivedError(),
+                kafkaTopics.getOrderReceivedError(),
+                getOrderReceived());
+        orderReceivedProducer.send(producerRecord).get();
+        orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().await(30, TimeUnit.SECONDS);
+
+        // Get order received from retry topic
+        OrderReceived actual = KafkaTestUtils.getSingleRecord(orderReceivedRetryConsumer, kafkaTopics.getOrderReceivedRetry(), 30000).value();
+
+        // then
+        assertEquals(0, orderMessageErrorConsumerAspect.getAfterOrderConsumedEventLatch().getCount());
+        assertNotNull(actual);
+        assertEquals(getOrderReference(), actual.getOrderUri());
+    }
+
+    private OrderReceived getOrderReceived() {
+        OrderReceived orderReceived = new OrderReceived();
+        orderReceived.setOrderUri(getOrderReference());
+        return orderReceived;
+    }
+
+    private String getOrderReference() {
+        return "/orders/ORD-111111-" + orderId;
+    }
+}
