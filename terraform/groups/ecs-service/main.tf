26f8eb4ab32cf936ded989fb3f53904f3d9a725e
drobledomunoz-ch <drobledomunoz@companieshouse.gov.uk>
2024-09-24 10:19:32 +0100

    remove references for load balancer

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -43,11 +43,6 @@ module "ecs-service" {
   healthcheck_path               = local.healthcheck_path
   healthcheck_matcher            = local.healthcheck_matcher
 
-  # Load balancer configuration
-  lb_listener_arn                 = data.aws_lb_listener.service_lb_listener.arn
-  lb_listener_rule_priority       = local.lb_listener_rule_priority
-  lb_listener_paths               = local.lb_listener_paths
-
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
