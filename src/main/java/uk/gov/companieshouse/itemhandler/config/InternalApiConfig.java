1dde74f28c1fc8b09d34a142ed415fb3ed2a39bc
jhalladey-ch <jhalladey@companieshouse.gov.uk>
2025-07-18 13:00:19 +0100

    initial commit

--- src/main/java/uk/gov/companieshouse/itemhandler/config/InternalApiConfig.java
+++ src/main/java/uk/gov/companieshouse/itemhandler/config/InternalApiConfig.java
@@ -1,19 +1,24 @@
 package uk.gov.companieshouse.itemhandler.config;
 
 import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
+import uk.gov.companieshouse.itemhandler.client.ApiClient;
 
 @Configuration
 public class InternalApiConfig {
 
+    @Autowired
+    ApiClient apiClient;
+
     @Bean("internalApiClient")
     Supplier<InternalApiClient> internalApiClientSupplier(@Value("${chs.kafka.api.url}") final String chsKafkaApiUrl) {
         return () -> {
-            InternalApiClient internalApiClient = new InternalApiClient(new ApiKeyHttpClient(""));
+            InternalApiClient internalApiClient = apiClient.getInternalApiClient();
             internalApiClient.setBasePath(chsKafkaApiUrl);
 
             return internalApiClient;
